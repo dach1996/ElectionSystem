fc4197b34bc137bf2561b22841d03e13fe7560b9
Danny Cardenas <dach19962012@gmail.com>
2021-06-06 17:08:41 -0500

    Create Angular Page

---
+++ Dach.ElectionSystem.Angular/ElectionSystem/src/app/forggote-password/forggote-password.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forggote-password',
+  templateUrl: './forggote-password.component.html',
+  styleUrls: ['./forggote-password.component.css']
+})
+export class ForggotePasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
